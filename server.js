57a851750eaa0e2c3d38f601146ccd71c5f6ee3c
abatheone1 <arbabacar@gmail.com>
2021-05-15 22:21:40 +0000

    server modified - routing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('2021.js');
+const router = jsonServer.router('2021.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
